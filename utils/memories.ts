18379004a09d4a3ff0036afe1609562e15465207
hsintao <hsintao@tencent.com>
2025-07-30 17:00:11 +0800

    ✨ feat(utils): 实现函数记忆化及LRUCache缓存机制

---
+++ utils/memories.ts
@@ -0,0 +1,81 @@
+import type { AnyFunc } from "~types/index";
+
+interface cache<K=any,V=any>{
+    get(k:K):V|null
+    put(k:K,v:V):void
+    has(k:K):boolean
+}
+interface memories<Fn extends AnyFunc>{
+    (this:ThisParameterType<Fn>,...args:Parameters<Fn>):ReturnType<Fn>;
+    cache:cache;
+}
+class LRUCache<K=any,V=any> implements cache<K,V>{
+    private map:Map<K,{value:V,timeStamp:number}>=new Map();
+    capacity:number
+    maxAge:number
+    constructor({capacity=10,maxAge=60*1000}){
+        this.capacity=capacity;
+        this.maxAge=maxAge;
+    }
+    get(k:K){
+        const v=this.map.get(k)
+        if(!v) return null;
+        this.map.delete(k);
+        if(Date.now()-v.timeStamp>this.maxAge) return null
+        this.map.set(k,v);
+        return v.value;
+    }
+    put(k:K,v:V){
+       if(this.map.has(k)){
+        this.map.delete(k);
+       }
+       this.map.set(k,{value:v,timeStamp:Date.now()});
+       if(this.map.size>this.capacity){
+           const oldestKey=this.map.keys().next().value;
+           this.map.delete(oldestKey!);
+       }
+    }
+    has(k:K){   
+        return this.map.has(k);
+    }   
+}
+/**
+ * 
+ * @param fn 需要记忆化的函数
+ * @param resolver 用于生成key的函数，如果不提供则使用第一个参数作为key
+ 
+ * @returns 缓存后的函数
+ */
+function memories<Fn extends AnyFunc>(fn:Fn,resolver:AnyFunc|null=null):memories<Fn>{
+    if(!(fn instanceof Function)) throw new Error('fn must be a function');
+    if(!(resolver instanceof Function)) throw new Error('resolver must be a function');
+    const cache:cache=new LRUCache({capacity:3});
+    const memozied:memories<Fn>=function(this,...args:Parameters<Fn>){
+        const key=resolver?resolver.call(this,...args):args[0];
+        if(cache.has(key)){
+            return cache.get(key);
+        }else{
+            const res=fn.call(this,...args);
+            cache.put(key,res);
+            return res;
+        }
+     
+    }
+    memozied.cache=cache;   
+    return memozied;
+}
+export default memories;
+
+// test
+const fib={
+    fibonacci(n:number):number{
+        if(n<=1) return n;
+        return this.fibonacci(n-1)+this.fibonacci(n-2);
+    }
+}
+const memoziedFibonacci=memories(fib.fibonacci,(n:number)=>n);
+memoziedFibonacci.cache=new LRUCache({capacity:3});
+fib.fibonacci=memoziedFibonacci;
+console.time('memoziedFibonacci');
+console.log(fib.fibonacci(10000));
+console.timeEnd('memoziedFibonacci');
